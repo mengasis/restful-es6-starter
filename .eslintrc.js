95248cef285a1399322fb160359d39d6b0261c6e
Manuel Venegas <mengasis@outlook.com>
2017-05-18 13:37:10 -0400

    add config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true
+	},
+	"globals": {
+		"autoload": true
+
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	"rules": {
+		"no-console": 0,
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"never"
+		]
+	}
+};
